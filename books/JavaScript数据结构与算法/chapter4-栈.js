4ea198a142fed30ef1f04eb07b44671673682f8d
fan.hu <fan.hu3@envision-digital.com>
2023-07-14 17:42:03 +0800

    update notes

---
+++ books/JavaScript数据结构与算法/chapter4-栈.js
@@ -0,0 +1,52 @@
+// @ts-nocheck
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = '';
+  while (number > 0) {
+    // {1}
+    rem = Math.floor(number % 2); // {2}
+    remStack.push(rem); // {3}
+    number = Math.floor(number / 2); // {4}
+  }
+  while (!remStack.isEmpty()) {
+    // {5}
+    binaryString += remStack.pop().toString();
+  }
+  return binaryString;
+}
+
+console.log(decimalToBinary(233)); // 11101001
+console.log(decimalToBinary(10)); // 1010
+console.log(decimalToBinary(1000)); // 1111101000
